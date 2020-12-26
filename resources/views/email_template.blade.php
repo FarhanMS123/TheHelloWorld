89a86adb4eca9cad3df0a02f1a9d304f6f11ff73
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2020-12-26 16:31:08 +0700

    Mail template with added variable

---
+++ resources/views/email_template.blade.php
@@ -0,0 +1,3 @@
+<p>Hi, my name is {{$data['name']}}</p>
+<p>I have some queries regarding the event. {{$data['text']}}</p>
+<p>Thank you for going through this feedback, you can reply to me at {{$data['email']}}</p>
