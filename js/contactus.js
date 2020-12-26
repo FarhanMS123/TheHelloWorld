9c479f4f04b96883711f66b5511d987dc17bac4a
astriddwrn <astrid.dewi.rana@gmail.com>
2020-12-27 00:28:18 +0700

    -

--- js/contactus.js
+++ js/contactus.js
@@ -1,15 +1,46 @@
 $(document).ready(function(){
     $(".invalidEmail").hide();
+    $(".invalidName").hide();
+    $(".invalidSubject").hide();
+    $(".invalidMessage").hide();
 
     $("#email").change(function(){
-        var VAL = this.value;
-        var validEmail = new RegExp('/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/');
-        if (validEmail.test(VAL)) {
+        var VALemail = this.value;
+        var validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+        if (validEmail.test(VALemail)) {
             $(".invalidEmail").hide();
         }
         else{
             $(".invalidEmail").show();
         }
+
+    });
+    $("#name").change(function(){
+        var VALname = this.value;
+        var validName = /^[a-zA-Z\s]{3,}$/;
+
+        if (validName.test(VALname)) {
+            $(".invalidName").hide();
+        }
+        else{
+            $(".invalidName").show();
+        }
+
+    });
+    $("#message").change(function(){
+        var VALmessage = this.value;
+        var validMessage = /^.{15,}$/;
+
+        if (validMessage.test(VALmessage)) {
+            $(".invalidMessage").hide();
+            $("#message").removeClass("invalid-active");
+        }
+        else{
+            $(".invalidMessage").show();
+            $("#message").addClass("invalid-active");
+        }
+
     });
 
     // $("#email").on("input", function(){
