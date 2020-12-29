836f7d6f93c89ce6e6a989ae6cd509cbfb1ab83d
astriddwrn <astrid.dewi.rana@gmail.com>
2020-12-30 02:15:39 +0700

    admin payment added
    jquery finished

--- Admin/js/javascript-admin.js
+++ Admin/js/javascript-admin.js
@@ -1,19 +1,76 @@
+
+// window.onload = function () {
+//     document.getElementById("view").onclick = function () {
+//         var overlay = document.getElementById("overlay");
+//         var popup = document.getElementById("popup");
+//         overlay.style.display = "block";
+//         popup.style.display = "block";
+//     };
+
+//     document.getElementById("closepopup").onclick = function () {
+//         var overlay = document.getElementById("overlay");
+//         var popup = document.getElementById("popup");
+//         overlay.style.display = "none";
+//         popup.style.display = "none";
+//     }
+// };
+
 $(document).ready(function(){
 
-    $(".linkPayment").click(function(){
-        window.location.href='/Admin/index-payment-admin.html';
-    });
+  
 
-    $(".linkParticipant").click(function(){
-        window.location.href='/Admin/html/participant-data-admin.html';;
-    });
+    $(".bukti").hide();
+    $(".rejected").hide();
+    $(".verified").hide();
 
-    $("#viewData").click(function(){
-        window.location.href='/Admin/html/view-data-admin.html';
+    $(".view").click(function(){ 
+        $(this).parents(".box").find(".bukti").addClass("popup").show();
+        $(".overlay").css("display", "block");
     });
+        
+    $(".overlay").click(function(){
+        $(".bukti").css("display", "none");
+        $("img").removeClass("popup");
+        $(".overlay").css("display", "none");
+        $(".cd-popup").css("display", "none");
+    });
+   
+    $(".verify").click(function(){
+        let thisVerify = this;
 
-    $("#editData").click(function(){
-        window.location.href='/Admin/html/edit-data-admin.html';
+        $(".alertText").text("Verify Payment for " + $(this).parents(".box").find(".groupName").html());
+        $(".cd-popup").css("display", "block");
+        $(".overlay").css("display", "block");
+
+        $(".yes").click(function(){
+            $(thisVerify).parents(".box").find(".status").hide();
+            $(thisVerify).parents(".box").find(".verified").show();
+            $(".overlay").trigger("click");
+            thisVerify=null;
+        })
+        $(".no").click(function(){
+            $(thisVerify).parents(".box").find(".status").hide();
+            $(thisVerify).parents(".box").find(".rejected").show();
+            $(".overlay").trigger("click");
+            thisVerify=null;
+        })
+        
+
+
+
+
+        // var ok = confirm('Verify Payment for Nama Team ' + $(this).parents(".box").find(".groupName").val());
+        // if(ok){
+        //     $(this).parents(".box").find(".status").hide();
+        //     $(this).parents(".box").find(".verified").show();
+            
+        // }
+        // else{
+        //     $(this).parents(".box").find(".status").hide();
+        //     $(this).parents(".box").find(".rejected").show();
+        // }
+
     });
+    
 
 });
