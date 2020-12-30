4768a496d67c6b042bc36e8da21f804329fcd335
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2020-12-30 21:01:33 +0700

    Moved Mail Logic to IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Email;
 
 class IndexController extends Controller
 {
     public function index(){
         //
+    }
+    
+    public function send(Request $request) {
+        $request->validate([
+            'name'=>'required|min:3',
+            'email'=>'required|email',
+            'subject'=>'required',
+            'text'=>'required|min:15'
+        ]);
+
+        $data = array(
+            'name'=>$request->name,
+            'subject'=>$request->subject,
+            'email'=>$request->email,
+            'text'=>$request->text
+        );
+        //Fill in email destination
+        Mail::to('///')->send(new Email($data));
+        return back()->with('success', 'Thank you for contacting us!');
     }
 }
