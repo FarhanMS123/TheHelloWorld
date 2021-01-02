c89064d041475df5fe018d8176086b08610d0c2f
Alfian <alfian101@gmail.com>
2021-01-02 22:13:53 +0700

    faq done

---
+++ js/faq.js
@@ -0,0 +1,155 @@
+/* Left Animate */
+/* 1 */
+$(document).ready(function(){
+    $("#toggle1").click(function () {
+      $("#toggle1-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle1").click(function(){
+        $("#tutorial1").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 2 */
+$(document).ready(function(){
+    $("#toggle2").click(function () {
+      $("#toggle2-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle2").click(function(){
+        $("#tutorial2").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 3 */
+$(document).ready(function(){
+    $("#toggle3").click(function () {
+      $("#toggle3-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle3").click(function(){
+        $("#tutorial3").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 4 */
+$(document).ready(function(){
+    $("#toggle4").click(function () {
+      $("#toggle4-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle4").click(function(){
+        $("#tutorial4").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 5 */
+$(document).ready(function(){
+    $("#toggle5").click(function () {
+      $("#toggle5-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle5").click(function(){
+        $("#tutorial5").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* Right */
+/* 1 */
+$(document).ready(function(){
+    $("#toggle6").click(function () {
+      $("#toggle6-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle6").click(function(){
+        $("#tutorial6").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 7 */
+$(document).ready(function(){
+    $("#toggle7").click(function () {
+      $("#toggle7-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle7").click(function(){
+        $("#tutorial7").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 8 */
+$(document).ready(function(){
+    $("#toggle8").click(function () {
+      $("#toggle8-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle8").click(function(){
+        $("#tutorial8").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 9 */
+$(document).ready(function(){
+    $("#toggle9").click(function () {
+      $("#toggle9-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle9").click(function(){
+        $("#tutorial9").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 10 */
+$(document).ready(function(){
+    $("#toggle10").click(function () {
+      $("#toggle10-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle10").click(function(){
+        $("#tutorial10").toggleClass("tutorial-subtitle");
+      });
+});
+
+/* 11 */
+$(document).ready(function(){
+    $("#toggle11").click(function () {
+      $("#toggle11-content").animate({
+        height: 'toggle'
+      });
+    });
+});
+$(document).ready(function(){
+    $("#toggle11").click(function(){
+        $("#tutorial11").toggleClass("tutorial-subtitle");
+      });
+});
