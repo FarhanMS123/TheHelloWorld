cde84d0ea0d47d11a7eac4f5ed4b3d13adc32122
Alfian <alfian101@gmail.com>
2021-01-02 23:19:02 +0700

    moved payment js

--- User/scriptUser.js
+++ User/scriptUser.js
@@ -62,49 +62,6 @@ $(document).ready(function(){
       });
 });
 
-/* Are You Sure??? */
-function myFunction() {
-  var txt;
-  var r = confirm("Are you sure?");
-  if (r == true) {
-    $(document).ready(function () {
-      /* Submit Change to View */
-        $("#before-submit").hide();
-        $("#after-submit").show();
-    });
-  } else {
-  }
-}
-/* Countdown Timer */
-// Set the date we're counting down to
-var countDownDate = new Date("Sept 15, 2021 23:59:59").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("earlybird").innerHTML = days + " hari " + hours + " jam "
-  + minutes + " menit " + seconds + " detik ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("earlybird").innerHTML = "Waktu Early Bird Sudah Selesai";
-  }
-}, 1000);
-
 /* Member Registration Validation */
 $(document).ready(function(){
 
