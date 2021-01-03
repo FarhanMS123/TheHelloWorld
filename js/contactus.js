50cb163c10032e0a13fb7bcc2439e9b6488ab981
astriddwrn <astrid.dewi.rana@gmail.com>
2021-01-03 22:54:03 +0700

    update index
    navbar highlight, bug fixed

--- js/contactus.js
+++ js/contactus.js
@@ -1,46 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function($){
 
-    //letters format with spaces
-    $.validator.addMethod("letters", function(value, element) {
-        return this.optional(element) || value == value.match(/^[a-zA-Z\s]*$/);
-    });
+    
+    $("form").each(function() {
 
-    $("form[name='contactus']").validate({
-        rules:{
-            name:{
-                required: true,
-                minlength: 3,
-                letters: true
+
+        //letters format with spaces
+        $.validator.addMethod("letters", function(value, element) {
+            return this.optional(element) || value == value.match(/^[a-zA-Z\s]*$/);
+        });
+
+        $(this).validate({
+            rules:{
+                name:{
+                    required: true,
+                    minlength: 3,
+                    letters: true
+                },
+                email:{
+                    required: true,
+                    email: true,
+                },
+                subject:{
+                    required: true,
+                    minlength:3
+                },
+                message:{
+                    required: true,
+                    minlength: 15
+                }
             },
-            email:{
-                required: true,
-                email: true,
+            messages:{
+                name: "*Please specify your name. Only letters and spaces are allowed and minimum 3 characters.",
+                email: "*Please specify your email. For example(someone@example.com).",
+                subject: "*Please specify your subject. Minimum 3 characters.",
+                message: "*please specify your message. Minimum 15 characters."
             },
-            subject:{
-                required: true,
-                minlength:3
-            },
-            message:{
-                required: true,
-                minlength: 15
+
+            //for error css div.error
+            errorElement : 'div',
+
+            submitHandler: function(form) {
+                alert("Your message has been submited successfully.");
+                form.submit();
+                
             }
-        },
-        messages:{
-            name: "*Please specify your name. Only letters and spaces are allowed and minimum 3 characters.",
-            email: "*Please specify your email. For example(someone@example.com).",
-            subject: "*Please specify your subject. Minimum 3 characters.",
-            message: "*please specify your message. Minimum 15 characters."
-        },
 
-        //for error css div.error
-        errorElement : 'div',
-
-        submitHandler: function(form) {
-            alert("Your message has been submited successfully.");
-            form.submit();
-            
-        }
-
+        });
     });
 
 });
