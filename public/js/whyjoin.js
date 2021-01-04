9ebb9d1a4cf8e1436e2e931014edc024f083dd10
Gregory Hugo <70943340+greg-hugo@users.noreply.github.com>
2021-01-04 21:41:07 +0700

    Fixed whyjoin script

--- public/js/whyjoin.js
+++ public/js/whyjoin.js
@@ -1,13 +1,50 @@
-$(document).ready(function(){
-    $("#whyjoin-onclick1").click(function () {
-      $("#whyjoin-content-drop1").animate({
+var a = 0;
+var b = 0;
+var c = 0;
+$(document).ready(function () {
+  $("#whyjoin-onclick1").click(function () {
+    if (b == 1) {
+      $("#whyjoin-content-drop2").animate({
         height: 'toggle'
       });
+      b = 0;
+    }
+    if (c == 1) {
+      $("#whyjoin-content-drop3").animate({
+        height: 'toggle'
+      });
+      c = 0;
+    }
+    if (a == 0) {
+      a = 1;
+    } else if (a == 1) {
+      a = 0;
+    }
+    $("#whyjoin-content-drop1").animate({
+      height: 'toggle'
+    });
     });
 });
 
 $(document).ready(function(){
   $("#whyjoin-onclick2").click(function () {
+    if (a == 1) {
+      $("#whyjoin-content-drop1").animate({
+        height: 'toggle'
+      });
+      a = 0;
+    }
+    if (c == 1) {
+      $("#whyjoin-content-drop3").animate({
+        height: 'toggle'
+      });
+      c = 0;
+    }
+    if (b == 0) {
+      b = 1;
+    } else if (b == 1) {
+      b = 0;
+    }
     $("#whyjoin-content-drop2").animate({
       height: 'toggle'
     });
@@ -16,6 +53,23 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#whyjoin-onclick3").click(function () {
+    if (a == 1) {
+      $("#whyjoin-content-drop1").animate({
+        height: 'toggle'
+      });
+      a = 0;
+    }
+    if (b == 1) {
+      $("#whyjoin-content-drop2").animate({
+        height: 'toggle'
+      });
+      b = 0;
+    }
+    if (c == 0) {
+      c = 1;
+    } else if (c == 1) {
+      c = 0;
+    }
     $("#whyjoin-content-drop3").animate({
       height: 'toggle'
     });
@@ -31,10 +85,9 @@ $(document).ready(function () {
         $("#whyjoin-content-drop2").hide();
         $("#whyjoin-content-drop3").hide();
       } else if (scroll > 2100 && scroll < 9150 && y == 0) {
-        $("#whyjoin-content-drop1").slideDown();
         $("#whyjoin-content-drop2").slideDown();
-        $("#whyjoin-content-drop3").slideDown();
         y = 1;
+      b = 1;
       }
   });
 });
