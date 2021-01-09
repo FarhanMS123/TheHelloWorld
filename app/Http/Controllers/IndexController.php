c20dbde1e95930d752a8cc5c518e32ea78e02c3b
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-01-09 20:22:08 +0700

    Fix css prizes url

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,12 +6,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\Email;
 
+// use Illuminate\Support\Facades\View;
+// use Illuminate\Http\Response;
+
 class IndexController extends Controller
 {
     public function index(){
         return view('index');
     }
-    
+
+    public function css_championPrizes(){
+        return response(view('css.championPrizes'))->header('Content-Type', 'text/css');
+        // return view("css.championPrizes");
+    }
+
     public function send(Request $request) {
         $request->validate([
             'name'=>'required|min:3',
